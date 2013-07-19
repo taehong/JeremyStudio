a35a9058534f5ab5d86e96efb7a1ef42b0fe1b9a
Taehong Jeremy Jeong <enjoyjake4u@gmail.com>
2013-07-19 15:23:03 +0900

    2013.07.19 init

--- JeremyStudio/JeremyStudio.js
+++ JeremyStudio/JeremyStudio.js
@@ -2,58 +2,71 @@
 window.onload = function() {
 	JeremyStudio.init();
 }
-function JS(address) {
-	if (address === 'AssetManager') {
-		return {
-			get : (function() {
-				return JeremyStudio.AssetManager.getAsset;
-			})(),
-			type : (function() {
-				return function(typeName) {
-					return JeremyStudio.AssetManager.assetType[typeName];
-				};
-			})(),
-			set : (function() {
-				return JeremyStudio.AssetManager.addResource;
-			})
-		};
-	} else if (address === 'DataManager') {
-		return {
-			set : (function() {
-				return JeremyStudio.DataManager.setData;
-			})(),
-			get : (function() {
-				return JeremyStudio.DataManager.getData;
-			})()
-		};
-	} else if (address === 'Renderer2D') {
-		return {
-			render : (function() {
-				return JeremyStudio.Renderer2D.render;
-			})(),
-			add : (function() {
-				return JeremyStudio.Renderer2D.addRenderable;
-			})(),
-			remove : (function() {
-				return JeremyStudio.Renderer2D.removeRenderable;
-			})()
-		};
-	} else if (address === 'SceneManager') {
-		return {
-			add : (function() {
-				return JeremyStudio.SceneManager.addScene;
-			})(),
-			play : (function() {
-				return JeremyStudio.SceneManager.playScene;
-			})(),
-			next : (function() {
-				return JeremyStudio.SceneManager.playNext;
-			})(),
-			prev : (function() {
-				return JeremyStudio.SceneManager.playPrev;
-			})()
-		};
+function JS(manager) {
+	var interfaceObject = null;
+	switch (manager) {
+		case 'AssetManager':
+			interfaceObject = {
+				get : (function() {
+					return JeremyStudio.AssetManager.getAsset;
+				})(),
+				type : (function() {
+					return function(typeName) {
+						return JeremyStudio.AssetManager.assetType[typeName];
+					};
+				})(),
+				set : (function() {
+					return JeremyStudio.AssetManager.addResource;
+				})
+			};
+			break;
+		case 'DataManager':
+			interfaceObject = {
+				set : (function() {
+					return JeremyStudio.DataManager.setData;
+				})(),
+				get : (function() {
+					return JeremyStudio.DataManager.getData;
+				})()
+			};
+			break;
+		case 'SceneManager':
+			interfaceObject = {
+				add : (function() {
+					return JeremyStudio.SceneManager.addScene;
+				})(),
+				play : (function() {
+					return JeremyStudio.SceneManager.playScene;
+				})(),
+				next : (function() {
+					return JeremyStudio.SceneManager.playNext;
+				})(),
+				prev : (function() {
+					return JeremyStudio.SceneManager.playPrev;
+				})()
+			};
+			break;
+		case 'SoundManager':
+
+			break;
+		case 'Renderer2D':
+			interfaceObject = {
+				render : (function() {
+					return JeremyStudio.Renderer2D.render;
+				})(),
+				add : (function() {
+					return JeremyStudio.Renderer2D.addRenderable;
+				})(),
+				remove : (function() {
+					return JeremyStudio.Renderer2D.removeRenderable;
+				})()
+			};
+			break;
+		case 'Renderer3D':
+		
+			break;
 	}
+	return interfaceObject;
 }
 
 var JeremyStudio = {
