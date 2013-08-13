a88fc5d50e3c8ca1df67130f9c993bc888fb33af
Taehong Jeremy Jeong <enjoyjake4u@gmail.com>
2013-08-13 19:57:04 +0900

    Editor Init

---
+++ JeremyFramework/game/scene/Editor/EditorMain.js
@@ -0,0 +1,15 @@
+/**
+ * @author Jeremy Jeong
+ */
+J('STU')('Context').add(J('LIB')('SceneContext')({
+    name : 'EditorMain',
+    initCB : function() {
+        
+    },
+    updateCB : function() {
+
+    },
+    destroyCB : function() {
+        
+    }    
+}));
