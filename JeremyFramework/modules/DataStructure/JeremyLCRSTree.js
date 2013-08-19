51c8dc9ce67cafed7ccf2af2e87a4bb37af2e115
Taehong Jeremy Jeong <enjoyjake4u@gmail.com>
2013-08-20 03:45:38 +0900

    LCRSTree, LinkedList

--- JeremyFramework/modules/DataStructure/JeremyLCRSTree.js
+++ JeremyFramework/modules/DataStructure/JeremyLCRSTree.js
@@ -1,25 +1,48 @@
 /**
  * @author Jeremy
  */
-// TODO: sibling
+// TODO: Tree, Node 구조로 바꾸기
 function JeremyLCRSTree(argo) {
-    this.parent = argo.parent;
-    this.key = argo.key;
-    this.value = argo.value;
+    this.id = Math.round((new Date()).getTime() * Math.random());
+    if (argo !== undefined) {
+        this.parent = argo.parent || null;
+        this.key = argo.key || null;
+        this.value = argo.value || null;
+    } else {
+        this.parent = null;
+        this.key = null;
+        this.value = null;
+    }
     this.left = new JeremyLinkedList();
     this.right = new JeremyLinkedList();
 };
+// JeremyLCRSTree.prototype.toString = function() {
+    // var tokens = {
+        // id : "id : " + this.id,
+        // key : "key : " + (this.key !== null ? this.key : 'null'),
+        // value : "value : " + (this.value !== null ? this.value : 'null'),
+        // parent : "parent : " + (this.parent !== null ? this.parent.id : 'null'),
+        // child : "child : " + (this.left.isEmpty() ? 'null' : this.left.toString()),
+        // // sibling : "sibling : " + (this.right.isEmpty() ? 'null' : this.right.toString())
+    // };
+    // // return "{" + tokens.id + ", " + tokens.key  + ", " + tokens.value + ", " + tokens.parent + ", " + tokens.child + ", " + tokens.sibling + "}";
+    // return "{" + tokens.id + ", " + tokens.key  + ", " + tokens.value + ", " + tokens.parent + ", " + tokens.child + "}";
+// };
 JeremyLCRSTree.prototype.appendChild = function(key, value) {
     var newChild = new JeremyLCRSTree({
         parent : this,
         key : key,
         value : value
     });
-    this.left.forwards(function (node, list, argo) {
+    this.left.forwards(function(node, list, argo) {
         argo.newChild.addSibling(node);
-        node.addSibling(argo.newChild);
+        node.getItem().addSibling(argo.newChild);
     }, {
-        newChild:newChild
+        newChild : newChild
     });
     this.left.insertEnd(newChild);
+};
+JeremyLCRSTree.prototype.addSibling = function(node) {
+    if (!this.right.containsNode(node))
+        this.right.insertEnd(node);
 };
--- JeremyFramework/modules/DataStructure/JeremyLinkedList.js
+++ JeremyFramework/modules/DataStructure/JeremyLinkedList.js
@@ -2,10 +2,63 @@
  * @author Jeremy
  */
 function JeremyLinkedList() {
+    this.length = 0;
     this.front = null;
     this.end = null;
 }
 
+JeremyLinkedList.prototype.isEmpty = function() {
+    return this.length === 0;
+};
+JeremyLinkedList.prototype.toString = function() {
+    return this.forwards(function(node, list, argo) {
+        argo.string += node.toString();
+        if (node.hasNext()) {
+            argo.string += ", ";
+        } else {
+            return argo.string += "]";
+        }
+    }, {
+        string : "["
+    });
+};
+JeremyLinkedList.prototype.containsNode = function(node) {
+    return this.forwards(function(node, list, argo) {
+        if (node.isEquals(argo.given)) {
+            return true;
+        }
+    }, {
+        given : node
+    });
+};
+JeremyLinkedList.prototype.containsValue = function(value, compare) {
+    return this.forwards(function(node, list, argo) {
+        if (argo.compare(node.item, argo.given)) {
+            return true;
+        }
+    }, {
+        given : value,
+        compare : (compare !== undefined ? compare : function(a, b) {
+            return a === b;
+        })
+    });
+};
+JeremyLinkedList.prototype.getNodesContainingItem = function(value, compare) {
+    return this.forwards(function(node, list, argo) {
+        if (argo.compare(node.item, argo.given)) {
+            argo.list.push(node);
+        }
+        if (!node.hasNext()) {
+            return argo.list;
+        }
+    }, {
+        given : value,
+        compare : (compare !== undefined ? compare : function(a, b) {
+            return a === b;
+        }),
+        list : []
+    });
+};
 // TODO: Iterator
 JeremyLinkedList.prototype.toArray = function() {
     var arr = this.forwards(function(node, list, argo) {
@@ -45,6 +98,7 @@ JeremyLinkedList.prototype.insertBefore = function(node, item) {
         this.front = newNode;
     }
     node.setPrev(newNode);
+    this.length++;
 };
 JeremyLinkedList.prototype.insertAfter = function(node, item) {
     var newNode = null;
@@ -56,11 +110,13 @@ JeremyLinkedList.prototype.insertAfter = function(node, item) {
         this.end = newNode;
     }
     node.setNext(newNode);
+    this.length++;
 };
 JeremyLinkedList.prototype.insertFront = function(item) {
     if (this.front === null) {
         this.front = new this.ListNode(item, null, null);
         this.end = this.front;
+        this.length++;
     } else {
         this.insertBefore(this.front, item);
     }
@@ -84,11 +140,25 @@ JeremyLinkedList.prototype.remove = function(node) {
         this.end = node.getPrev();
     }
     node.destroy();
+    this.length--;
 };
 JeremyLinkedList.prototype.ListNode = function(item, prev, next) {
+    this.id = Math.round((new Date()).getTime() * Math.random());
     this.item = item;
     this.prev = prev;
     this.next = next;
+};
+JeremyLinkedList.prototype.ListNode.prototype.toString = function() {
+    var tokens = {
+        id : "id : " + this.id,
+        item : "item : " + this.item,
+        prev : "prev : " + (this.prev !== null ? this.prev.id : 'null'),
+        next : "next : " + (this.next !== null ? this.next.id : 'null')
+    };
+    return "<" + tokens.id + ", " + tokens.item + ", " + tokens.prev + ", " + tokens.next + ">";
+};
+JeremyLinkedList.prototype.ListNode.prototype.isEquals = function(node) {
+    return this.id === node.id;
 };
 JeremyLinkedList.prototype.ListNode.prototype.setItem = function(item) {
     this.item = item;
@@ -121,7 +191,7 @@ JeremyLinkedList.prototype.ListNode.prototype.destroy = function() {
     this.item = this.prev = this.next = null;
 };
 (function() {
-    var target = (Jeremy != undefined ? Jeremy.getComponent('JeremyDataStructure') : undefined);
+    var target = (window['Jeremy'] != undefined ? Jeremy.getComponent('JeremyDataStructure') : undefined);
     if (target) {
         target.addModule('JeremyLinkedList', JeremyLinkedList);
     }
