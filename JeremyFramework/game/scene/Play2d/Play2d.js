66796e211b76b0ca9b5746be83c1d70e6124607c
Taehong Jeremy Jeong <enjoyjake4u@gmail.com>
2013-08-20 16:41:35 +0900

    Working On JeremySprite

--- JeremyFramework/game/scene/Play2d/Play2d.js
+++ JeremyFramework/game/scene/Play2d/Play2d.js
@@ -2,120 +2,115 @@
  * @author Jeremy Jeong
  */
 J('STU')('Context').add(J('LIB')('SceneContext')({
-    name : 'Play2d',
-    initCB : function() {
-        var canvas = J('STU')('R2D').canvas('effect');
-        this.map = J('LIB')('TileMap')({
-            aabb : J('MAT')('AABB2')({
-                center : J('MAT')('Vec3')({
-                    x : canvas.width / 2,
-                    y : canvas.height / 2,
-                    w : 1
-                }),
-                half : J('MAT')('Vec3')({
-                    x : canvas.height / 2,
-                    y : canvas.height / 2,
-                    w : 0
-                })
-            }),
-            number : 12
-        });
-        console.log(this.map);
-        this.effectVignette = J('LIB')('Renderable2D')({
-            layer : 'effect',
-            drawCB : function(ctx, argo) {
-                ctx.drawImage(argo.img, 0, 0);
-            },
-            argo : {
-                img : J('STU')('Asset').get('image', 'maskImgVignette').getImage()
-            }
-        });
-        this.effectNoise = J('LIB')('Renderable2D')({
-            layer : 'effect',
-            drawCB : function(ctx, argo) {
-                ctx.globalAlpha = 0.8;
-                ctx.drawImage(argo.img, 0, 0);
-                ctx.globalAlpha = 1.0;
-            },
-            argo : {
-                img : J('STU')('Asset').get('image', 'maskImgNoise').getImage()
-            }
-        });
-        this.effectScanline = J('LIB')('Renderable2D')({
-            layer : 'effect',
-            drawCB : function(ctx, argo) {
-                ctx.globalAlpha = 0.4;
-                ctx.drawImage(argo.img, 0, 0);
-                ctx.globalAlpha = 1.0;
-            },
-            argo : {
-                img : J('STU')('Asset').get('image', 'maskImgScanline').getImage()
-            }
-        });
-        this.characterMask = J('LIB')('Renderable2D')({
-            layer : 'game',
-            drawCB : function(ctx, argo) {
-                var mousePos = J('STU')('Data').get('mousePos') || {
-                    x : 0,
-                    y : 0
-                }, radius = argo.getRadius();
-                ctx.globalCompositeOperation = 'source-over';
-                ctx.globalAlpha = 0.5;
-                ctx.fillStyle = '#000000';
-                ctx.fillRect(0, 0, 730, 440);
-                ctx.globalCompositeOperation = 'destination-out';
-                ctx.globalAlpha = 1.0;
-                ctx.fillStyle = '#ffffff';
-                ctx.beginPath();
-                ctx.arc(mousePos.x, mousePos.y, radius, 0, 2 * Math.PI);
-                ctx.fill();
-                ctx.globalCompositeOperation = 'source-over';
-            },
-            argo : {
-                getRadius : function() {
-                    return 30 + 5 * Math.sin(J('STU')('Data').get('characterMaskAngle') / 180 * Math.PI)
-                }
-            }
-        });
-        $('#jeremy').bind('mousemove', function(e) {
-            J('STU')('Data').set('mousePos', {
-                x : e.offsetX,
-                y : e.offsetY
-            });
-        });
-        this.map.tiles.forEach(function(col, row, list, elem) {
-            J('STU')('R2D').add(elem.renderable);
-        });
-        J('STU')('R2D').add(this.effectVignette);
-        J('STU')('R2D').add(this.effectNoise);
-        J('STU')('R2D').add(this.effectScanline);
-        J('STU')('R2D').add(this.characterMask);
-        this.characterMaskAngle = 0;
-        J('STU')('Data').set('characterMaskAngle', this.characterMaskAngle);
+	name : 'Play2d',
+	initCB : function() {
+		var canvas = J('STU')('R2D').canvas('effect');
+		this.map = J('LIB')('TileMap')({
+			aabb : J('MAT')('AABB2')({
+				center : J('MAT')('Vec3')({
+					x : canvas.width / 2,
+					y : canvas.height / 2,
+					w : 1
+				}),
+				half : J('MAT')('Vec3')({
+					x : canvas.height / 2,
+					y : canvas.height / 2,
+					w : 0
+				})
+			}),
+			number : 12
+		});
+		console.log(this.map);
+		this.effectVignette = J('LIB')('Renderable2D')({
+			layer : 'effect',
+			drawCB : function(ctx, argo) {
+				ctx.drawImage(argo.img, 0, 0);
+			},
+			argo : {
+				img : J('STU')('Asset').get('image', 'maskImgVignette').getImage()
+			}
+		});
+		this.effectNoise = J('LIB')('Renderable2D')({
+			layer : 'effect',
+			drawCB : function(ctx, argo) {
+				ctx.globalAlpha = 0.8;
+				ctx.drawImage(argo.img, 0, 0);
+				ctx.globalAlpha = 1.0;
+			},
+			argo : {
+				img : J('STU')('Asset').get('image', 'maskImgNoise').getImage()
+			}
+		});
+		this.effectScanline = J('LIB')('Renderable2D')({
+			layer : 'effect',
+			drawCB : function(ctx, argo) {
+				ctx.globalAlpha = 0.4;
+				ctx.drawImage(argo.img, 0, 0);
+				ctx.globalAlpha = 1.0;
+			},
+			argo : {
+				img : J('STU')('Asset').get('image', 'maskImgScanline').getImage()
+			}
+		});
+		this.characterMask = J('LIB')('Renderable2D')({
+			layer : 'game',
+			drawCB : function(ctx, argo) {
+				var mousePos = J('STU')('Data').get('mousePos') || {
+					x : 0,
+					y : 0
+				}, radius = argo.getRadius();
+				ctx.globalCompositeOperation = 'source-over';
+				ctx.globalAlpha = 0.5;
+				ctx.fillStyle = '#000000';
+				ctx.fillRect(0, 0, 730, 440);
+				ctx.globalCompositeOperation = 'destination-out';
+				ctx.globalAlpha = 1.0;
+				ctx.fillStyle = '#ffffff';
+				ctx.beginPath();
+				ctx.arc(mousePos.x, mousePos.y, radius, 0, 2 * Math.PI);
+				ctx.fill();
+				ctx.globalCompositeOperation = 'source-over';
+			},
+			argo : {
+				getRadius : function() {
+					return 30 + 5 * Math.sin(J('STU')('Data').get('characterMaskAngle') / 180 * Math.PI)
+				}
+			}
+		});
+		$('#jeremy').bind('mousemove', function(e) {
+			J('STU')('Data').set('mousePos', {
+				x : e.offsetX,
+				y : e.offsetY
+			});
+		});
+		this.map.tiles.forEach(function(col, row, list, elem) {
+			J('STU')('R2D').add(elem.renderable);
+		});
+		J('STU')('R2D').add(this.effectVignette);
+		J('STU')('R2D').add(this.effectNoise);
+		J('STU')('R2D').add(this.effectScanline);
+		J('STU')('R2D').add(this.characterMask);
+		this.characterMaskAngle = 0;
+		J('STU')('Data').set('characterMaskAngle', this.characterMaskAngle);
 
-        this.girlSprite = J('LIB')('Renderable2D')({
-            layer : 'game',
-            drawCB : function(ctx, argo) {
-                argo.sprite.drawFrame(ctx, argo.posX, argo.posY, argo.pivot);
-            },
-            argo : {
-                sprite : {
-                    front : J('STU')('Asset').get('sprite', 'GirlSprite').setFrameSequence({
-                        sequence : ['GirlFrontIdle', 'GirlFrontStepLeft', 'GirlFrontIdle', 'GirlFrontStepRight'],
-                        loop : true,
-                        pingpong : false
-                    })
-                },
-                posX : 100,
-                posY : 100,
-                pivot : JeremySprite.ePivotType.kCenter
-            }
-        });
-        J('STU')('R2D').add(this.girlSprite);
-    },
-    updateCB : function() {
-        J('STU')('Data').set('characterMaskAngle', this.characterMaskAngle += 3);
-    },
-    destroyCB : function() {
-    }
+		this.girlSprite = J('STU')('Asset').get('sprite', 'GirlSprite').setCurrentSequence('walkFront');
+		this.girlSpriteRenderable = J('LIB')('Renderable2D')({
+			layer : 'game',
+			drawCB : function(ctx, argo) {
+				argo.sprite.drawFrame(ctx, argo.posX, argo.posY, argo.pivot);
+			},
+			argo : {
+				sprite : this.girlSprite,
+				posX : 100,
+				posY : 100,
+				pivot : JeremySprite.ePivotType.kCenter
+			}
+		});
+		J('STU')('R2D').add(this.girlSpriteRenderable);
+	},
+	updateCB : function() {
+		J('STU')('Data').set('characterMaskAngle', this.characterMaskAngle += 3);
+	},
+	destroyCB : function() {
+	}
 }));
--- JeremyFramework/modules/Library/JeremySprite.js
+++ JeremyFramework/modules/Library/JeremySprite.js
@@ -1,78 +1,81 @@
 function JeremySprite(argo) {
-    this.atlas = argo.atlas;
-    this.data = argo.data;
-    this.frameSequence = null;
-    this.currentFrame = 0;
-    this.isLooping = false;
-    this.isPingpong = false;
+	this.atlas = argo.atlas;
+	this.data = argo.data;
+	this.sequence = argo.sequence;
+	this.currentSequence = null;
+	this.currentFrame = 0;
+	this.timer;
 }
 
 JeremySprite.ePivotType = {
-    kLeftTop : 0,
-    kCenter : 1
+	kLeftTop : 0,
+	kCenter : 1
 };
 JeremySprite.prototype.getWidth = function() {
-    if (!this.width) {
-        return this.width = this.data.frames[0].sourceSize.w;
-    } else {
-        return this.width;
-    }
+	if (!this.width) {
+		return this.width = this.data.frames[0].sourceSize.w;
+	} else {
+		return this.width;
+	}
 };
 JeremySprite.prototype.getHeight = function() {
-    if (!this.height) {
-        return this.height = this.data.frames[0].sourceSize.h;
-    } else {
-        return this.height;
-    }
+	if (!this.height) {
+		return this.height = this.data.frames[0].sourceSize.h;
+	} else {
+		return this.height;
+	}
 };
 JeremySprite.prototype.getFrameIndex = function(frameName) {
-    var frameIndex = -1, frames = this.data.frames;
-    frames.forEach(function(elem, index) {
-        if (elem.filename.split('.')[0] === frameName) {
-            frameIndex = index;
-        }
-    });
-    return frameIndex;
-};
-JeremySprite.prototype.setFrameSequence = function(argo) {
-    var sequence = argo.sequence, frameSequece = [], thisSprite = this;
-    sequence.forEach(function(elem) {
-        frameSequece.push(thisSprite.getFrameIndex(elem));
-    });
-    this.frameSequence = frameSequece;
-    this.isLooping = argo.loop;
-    this.isPingpong = argo.pingpong;
-    this.currentFrame = 0;
-    return this;
-};
-JeremySprite.prototype.drawFrame = function(ctx, posX, posY, pivot) {
-    var frameData = this.getNextFrameData(), frame = frameData.frame, x, y;
-    switch(pivot) {
-        case JeremySprite.ePivotType.kCenter:
-            x = posX - frame.w / 2;
-            y = posX - frame.h / 2;
-            break;
-        case JeremySprite.ePivotType.kLeftTop:
-            x = posX;
-            y = posY;
-            break;
-    }
-    ctx.drawImage(this.atlas.getImage(), frame.x, frame.y, frame.w, frame.h, x, y, frame.w, frame.h);
-    // TODO: Doing
+	var frameIndex = -1, frames = this.data.frames;
+	frames.forEach(function(elem, index) {
+		if (elem.filename.split('.')[0] === frameName) {
+			frameIndex = index;
+		}
+	});
+	return frameIndex;
 };
 JeremySprite.prototype.getCurrentFrameData = function() {
-    var frameData = this.data.frames[this.frameSequence[this.currentFrame]];
-    return frameData;
+	var frameData = this.data.frames[this.currentSequence[this.currentFrame]];
+	return frameData;
 };
 JeremySprite.prototype.getNextFrameData = function() {
-    var frameData = this.getCurrentFrameData();
-    this.currentFrame = (this.currentFrame + 1) % this.frameSequence.length;
-    // TODO: Looping, PingPong;
-    return frameData;
+	var frameData = this.getCurrentFrameData();
+	this.currentFrame = (this.currentFrame + 1) % this.frameSequence.length;
+	// TODO: Looping, PingPong;
+	return frameData;
+};
+JeremySprite.prototype.getSequence = function(name) {
+	var sequence = this.sequence[name];
+	if (sequence) {
+		return sequence
+	} else
+		return undefined;
+};
+JeremySprite.prototype.setCurrentSequence = function(name) {
+	var sequence = this.getSequence(name);
+	if (sequence) {
+		this.currentSequence = sequence;
+	}
+	return this;
+};
+JeremySprite.prototype.drawFrame = function(ctx, posX, posY, pivot) {
+	var frameData = this.getNextFrameData(), frame = frameData.frame, x, y;
+	switch(pivot) {
+		case JeremySprite.ePivotType.kCenter:
+			x = posX - frame.w / 2;
+			y = posX - frame.h / 2;
+			break;
+		case JeremySprite.ePivotType.kLeftTop:
+			x = posX;
+			y = posY;
+			break;
+	}
+	ctx.drawImage(this.atlas.getImage(), frame.x, frame.y, frame.w, frame.h, x, y, frame.w, frame.h);
+	// TODO: Doing
 };
 (function() {
-    var target = (Jeremy != undefined ? Jeremy.getComponent('JeremyLibrary') : undefined);
-    if (target) {
-        target.addModule('JeremySprite', JeremySprite);
-    }
+	var target = (Jeremy != undefined ? Jeremy.getComponent('JeremyLibrary') : undefined);
+	if (target) {
+		target.addModule('JeremySprite', JeremySprite);
+	}
 })();
