93411e61fbf54ef81f654b85592ac702d37ec48c
Taehong Jeremy Jeong <enjoyjake4u@gmail.com>
2013-09-20 03:57:36 +0900

    ObjectManager

--- JeremyFramework/game/scene/Playing/Playing.js
+++ JeremyFramework/game/scene/Playing/Playing.js
@@ -4,6 +4,8 @@
 J('STU')('Context').add(J('LIB')('SceneContext')({
 	name : 'Playing',
 	initCB : function() {
+		this.Jacqueline = J('STU')('Object').get(true, 'Jacqueline');
+		console.log(this.Jacqueline);
 	},
 	updateCB : function() {
 	},
