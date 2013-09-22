80a39c97661437d65f87711efc2406478ce21cb9
Taehong Jeremy Jeong <enjoyjake4u@gmail.com>
2013-09-23 07:04:55 +0900

    3D works

--- JeremyFramework/game/scene/Playing/Playing.js
+++ JeremyFramework/game/scene/Playing/Playing.js
@@ -4,26 +4,53 @@
 J('STU')('Context').add(J('LIB')('SceneContext')({
 	name : 'Playing',
 	initCB : function() {
+		var k = {
+			width : 730,
+			height : 440,
+			viewportSize : 100,
+			cameraNear : 0.0001,
+			cameraFar : 10000,
+			boxSize : 10,
+			vecOrigin : new THREE.Vector3(0, 0, 0)
+		};
 		// {
 		// canvas: J('STU')('Layer').layer('game').getCanvas()
 		// }
+		this.sampleMap = [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1];
 		this.R3D = {
-			camera : new THREE.PerspectiveCamera(45, 730 / 440, 1, 10000),
+			// camera : new THREE.PerspectiveCamera(45, 730 / 440, 1, 10000),
+			camera : new THREE.OrthographicCamera(-k.width / k.height * k.viewportSize, k.width / k.height * k.viewportSize, k.viewportSize, -k.viewportSize, k.cameraNear, k.cameraFar),
 			scene : new THREE.Scene(),
 			renderer : new THREE.CanvasRenderer(),
+			light : {},
 			geometry : {},
 			material : {},
 			mesh : {}
 		};
-		this.R3D.camera.position.z = 1000;
-		this.R3D.renderer.setSize(730, 440);
-		this.R3D.geometry.geoCube = new THREE.CubeGeometry(200, 200, 200);
+		this.R3D.camera.position.z = this.R3D.camera.position.y = this.R3D.camera.position.x = k.viewportSize;
+		this.R3D.camera.lookAt(k.vecOrigin);
+		this.R3D.renderer.setSize(k.width, k.height);
 		this.R3D.material.matCube = new THREE.MeshBasicMaterial({
-			color : 0xff0000,
-			wireframe : true
+			color : 0x0000ff,
+			wireframe : false
 		});
-		this.R3D.mesh.mesCube = new THREE.Mesh(this.R3D.geometry.geoCube, this.R3D.material.matCube);
-		this.R3D.scene.add(this.R3D.mesh.mesCube);
+		this.R3D.geometry.geoCube = new THREE.CubeGeometry(k.boxSize, k.boxSize, k.boxSize);
+		this.R3D.mesh.mesCubes = [];
+		for (var j = 0; j < Math.sqrt(this.sampleMap.length); j++) {
+			for (var i = 0; i < Math.sqrt(this.sampleMap.length); i++) {
+				if (this.sampleMap[j * Math.sqrt(this.sampleMap.length) + i] === 1) {
+					var aCube = new THREE.Mesh(this.R3D.geometry.geoCube, this.R3D.material.matCube);
+					aCube.position.x = i * k.boxSize;
+					aCube.position.z = j * k.boxSize;
+					this.R3D.mesh.mesCubes.push(aCube);
+				}
+			}
+		}
+		J('STU')('Data').set('R3D', this.R3D);
+		this.R3D.mesh.mesCubes.forEach(function(elem) {
+			J('STU')('Data').get('R3D').scene.add(elem);
+		});
+
 		// $('#game').remove();
 		$('#jeremy').append(this.R3D.renderer.domElement);
 
@@ -41,16 +68,11 @@ J('STU')('Context').add(J('LIB')('SceneContext')({
 		// }
 		// });
 		// mySwitch.doAction();
-
 	},
 	updateCB : function() {
 		this.INPUT.update();
 		this.JACQUELINE.update();
 		this.GAME.update();
-
-		this.R3D.mesh.mesCube.rotation.x += 0.01;
-		this.R3D.mesh.mesCube.rotation.y += 0.02;
-
 		this.R3D.renderer.render(this.R3D.scene, this.R3D.camera);
 	},
 	destroyCB : function() {
